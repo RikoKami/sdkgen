d57b0d5d502cbe4cd877c9cc4d5b8e11da6010d9
Larissa Dantas <orglary@gmail.com>
2021-09-24 00:20:52 -0400

    wip: starting dark theme

--- playground/src/containers/mainWrapper/index.tsx
+++ playground/src/containers/mainWrapper/index.tsx
@@ -1,19 +1,26 @@
 import { MainHeader } from "components/header";
 import * as React from "react";
 
+import useDarkMode from "../../hooks/useDarkMode";
 import s from "./mainWrapper.scss";
 
 interface Props {}
 
 function MainWrapper(props: React.PropsWithChildren<Props>): JSX.Element {
+  const { isDarkMode, toggle } = useDarkMode();
+  const theme = isDarkMode ? s.dark : s.light;
+
   return (
-    <div className={s.mainWrapper}>
-      <div className={s.top}>
+    <div className={`${s.mainWrapper} ${theme}`}>
+      <div className={`${s.top} ${theme}`}>
         <div className={s.constraint}>
           <MainHeader />
+          <button onClick={toggle} className={s.toggleTheme}>
+            <span className={s.icon}>{isDarkMode ? "🌙" : "☀️"}</span>
+          </button>
         </div>
       </div>
-      <div className={s.content}>
+      <div className={`${s.content} ${theme}`}>
         <div className={s.constraint}>
           <div className={s.wrapper}>{props.children}</div>
         </div>
